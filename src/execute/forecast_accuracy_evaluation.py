d6891715efbcfb2c6897922f5c1b8a1d715235c8
Wang Sihan <hessihan@gmail.com>
2021-05-14 12:30:36 +0900

    forecasting accuracy implemented

---
+++ src/execute/forecast_accuracy_evaluation.py
@@ -0,0 +1,70 @@
+# Evaluate predicted value (y_hat) for each methods.
+
+# build forecasting accuracy indicators table
+def accuracy_table(y_test, y_hats, indicators):
+    """
+    
+    Return a forecasting accuracy indicators table for each y_hats of forecasting methods.
+    
+    Parameters
+    ----------
+    y_test : pd.Series
+        The true data
+    y_hats : dict
+        The predicted data. {"name" : np.array}
+    indicators : dict
+        The forecasting accuracy functions. {"name" : function}
+        Each function is called from src/utils/accuracy.py
+
+    Returns
+    -------
+    pd.DataFrame
+        Forecasting accuracy indicators table.
+    
+    """
+    l = []
+    # loop for each accuracy indicators
+    for ind in indicators.keys():
+        m = []
+        # loop for each forecasting model
+        for model in y_hats.keys():
+            m.append(indicators[ind](y_test, y_hats[model]))
+        l.append(m)
+    
+    a = pd.DataFrame(l, 
+                     index=indicators.keys(),
+                     columns=y_hats.keys()
+                    )
+    return a
+
+if __name__ == "__main__":
+    # import external packages
+    import sys
+    import numpy as np
+    import pandas as pd
+    
+    # import internal modules
+    sys.path.insert(1, '../')
+    from utils.accuracy import *
+    
+    # read y_test cdv
+    y_test = np.loadtxt("../../assets/y_hats/y_test.csv", delimiter=',', skiprows=1, usecols=1)
+    
+    # read y_hat csv
+    y_hat_sarima_br = np.loadtxt("../../assets/y_hats/y_hat_sarima_br.csv", delimiter=',', skiprows=1, usecols=1)
+    y_hat_sarima_g = np.loadtxt("../../assets/y_hats/y_hat_sarima_g.csv", delimiter=',', skiprows=1, usecols=1)
+    y_hat_sarima_f = np.loadtxt("../../assets/y_hats/y_hat_sarima_f.csv", delimiter=',', skiprows=1, usecols=1)
+    
+    # cumpute forecast accuracy indicators
+    # accuracy function dict
+    ind_name = ["MAE", "MAPE", "MSE", "RMSE", "RMSPE"]
+    indicators = [MAE, MAPE, MSE, RMSE, RMSPE]
+    indicators = dict(zip(ind_name, indicators))
+    
+    # y_hats dictionary
+    method_name = ["SARIMA: BR", "SARIMA: G", "SARIMA: F"]
+    y_hats = [y_hat_sarima_br, y_hat_sarima_g, y_hat_sarima_f]
+    y_hats = dict(zip(method_name, y_hats))
+    
+    a = accuracy_table(y_test, y_hats, indicators)
+    print(a)
---
+++ src/execute/train_sarima_models.py
@@ -0,0 +1,89 @@
+# this is a kind of main execution file.
+# Call SARIMA.py and nn.py, using moving_window to predict and output fitted model list and y_hat vectors.
+# save trained models as pickle file.
+if __name__ == "__main__":
+    
+    # import external libraries
+    import sys
+    import numpy as np
+    import pandas as pd
+    import statsmodels.api as sm
+    import pickle
+    
+    # import internal modules
+    sys.path.insert(1, '../')
+    from models.sarima_multivariate import SARIMAModelList
+    from utils.ts_split import train_test_split
+
+    # read processed data
+    df = pd.read_csv("../../data/processed/dataset.csv")
+    
+    # column names
+    earning_v = df.columns[4: 10].values
+    account_v_bs = df.columns[11:].values
+    account_v_pl = df.columns[10:11].values
+    
+    # y
+    y = df[earning_v[-1]]
+    
+    # x 
+    x = df[np.append(account_v_bs, account_v_pl)]
+    
+    # time series train test split (4/5) : (1/5), yearly bases
+    y_train, y_test = train_test_split(y, ratio=(4,1))
+    x_train, x_test = train_test_split(x, ratio=(4,1))
+    
+    # save y_test as csv for calculating accuracy indicators
+    y_test.name = "y_test"
+    y_test.to_csv('../../assets/y_hats/y_test.csv')
+    
+    # Fit SARIMA
+    
+    # Brown & Rozeff
+    # call SARIMAModelList class
+    sarima_br = SARIMAModelList(order=(1, 0, 0), 
+                          seasonal_order=(0, 1, 1, 4), 
+                          y_prim_train=y_train,
+                          y_test=y_test,
+                          x_prim_train=x_train,
+                          x_test=x_test,
+                          silent=True
+                         )
+    y_hat_sarima_br = sarima_br.fit_rolling_window(len(y_train))
+    y_hat_sarima_br.name = "y_hat_sarima_br"
+    
+    # save y_hat as csv file
+    y_hat_sarima_br.to_csv('../../assets/y_hats/y_hat_sarima_br.csv')
+    
+    # save the class object as pickle file
+    pickle.dump(sarima_br, open('../../assets/trained_models/sarima_br.pkl', 'wb'))
+    # load pickle file
+    #loaded_model = pickle.load(open('../../assets/trained_models/sarima_br.pkl', 'rb'))
+    
+    # Griffin
+    sarima_g = SARIMAModelList(order=(0, 1, 1), 
+                          seasonal_order=(0, 1, 1, 4), 
+                          y_prim_train=y_train,
+                          y_test=y_test,
+                          x_prim_train=x_train,
+                          x_test=x_test,
+                          silent=True
+                         )
+    y_hat_sarima_g = sarima_g.fit_rolling_window(len(y_train))
+    y_hat_sarima_g.name = "y_hat_sarima_g"
+    y_hat_sarima_g.to_csv('../../assets/y_hats/y_hat_sarima_g.csv')
+    pickle.dump(sarima_g, open('../../assets/trained_models/sarima_g.pkl', 'wb'))
+    
+    # Foster
+    sarima_f = SARIMAModelList(order=(1, 0, 0), 
+                          seasonal_order=(0, 1, 0, 4), 
+                          y_prim_train=y_train,
+                          y_test=y_test,
+                          x_prim_train=x_train,
+                          x_test=x_test,
+                          silent=True
+                         )
+    y_hat_sarima_f = sarima_f.fit_rolling_window(len(y_train))
+    y_hat_sarima_f.name = "y_hat_sarima_f"
+    y_hat_sarima_f.to_csv('../../assets/y_hats/y_hat_sarima_f.csv')
+    pickle.dump(sarima_f, open('../../assets/trained_models/sarima_f.pkl', 'wb'))
